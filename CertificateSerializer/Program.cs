a8606bb630e9e1d4b81469d86d5b9b0944007b1c
athan isaac <athani@cs.washington.edu>
2019-09-07 16:16:48 -0700

    Add project files.

---
+++ CertificateSerializer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CertificateSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
